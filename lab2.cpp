fa31f9c741bf1ab9ee49ea6d35e78bb08c133d6d
Daiwik314 <68037582+Daiwik314@users.noreply.github.com>
2020-08-19 16:16:00 -0700

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -8,9 +8,9 @@ int hello1Function(string name1);
 int hello2Function(string name2);
 int main()
 {
-	string name1="your name1";
+	string name1="Corey Dickinson";
 	hello1Function(name1);
-	string name2="your name2";
+	string name2="Breighton Kohl";
 	hello2Function(name2);
 	return 0;
 }
