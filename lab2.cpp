d741fb1c5dee49730ac18a7dbdbcff55cf25d549
Daiwik314 <68037582+Daiwik314@users.noreply.github.com>
2020-08-19 16:25:40 -0700

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,16 +1,15 @@
 // Author: Corey Dickinson, Breighton Kohl
 // Description: Hello World in C++
 //============================================================
-main
+hello1Function
+/*
+* hello1Function.cpp
+*
+*/
 #include <iostream>
 using namespace std;
-int hello1Function(string name1);
-int hello2Function(string name2);
-int main()
+int hello1Function(string name1)
 {
-	string name1="Corey Dickinson";
-	hello1Function(name1);
-	string name2="Breighton Kohl";
-	hello2Function(name2);
-	return 0;
+cout << "hello World " << name1 << endl;
+return 0;
 }
